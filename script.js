c77c5f4d71fc348c37b35b5ca2eea33d97692163
Nadybee <natalie@yoodlize.com>
2022-05-26 18:52:52 -0600

    trying fetch fixes

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 var zipCode = 84097
 var ideas = document.querySelector(".idea")
 var restaurant = document.querySelector(".restaurant")
+
+var zipcodeInput= document.getElementById('zipcode')
+
+let zipcode=84097
+let openNow = false;
+let price = 2
+function setZipcode() {
+    
+    if(zipcodeInput.value.length ===5){
+        zipcode = parseInt(zipcodeInput.value)
+    }
+    else{
+        alert('please enter a valid zipcode')
+    }
+    console.log(zipcode)
+    zipcodeInput.value =''
+}
 // var apiKey = "Bearer mBoDH4rsoLue6XA8D1yQxqIWgXEuXNblkFNatJOaePgeVk5YMVB_X7fRfx2UG_7WrXCweMV0rAngdQ6DPHxLHe2Iqafgb6KVc1NklA3qpGL4ucfr1f28YQLfQ8iOYnYx"
 var apiKey =
   "Bearer HyNYhS-Wk9nc__m_elZsc3b9xAcu1I2Y0VcFoRw7XUMwWUXiwjjOAJU3jIYeFHgOyVHwGgkDE-Tcrp7k0ED2vdCXHAARiWKDylcxBQ-zm19OPXRgpRT_vt3O-1SNYnYx"
+var yelpURL =  `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=${zipcode}&price=${price}`
+// var yelpURL =  `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${zipcode}&price=${price}`
 
+
+function buildYelpURL() {
+    var yelpURL =  `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=84097`
+//   &price=2"
+
+}
 function fetchRestaurants() {
   var myHeaders = new Headers()
   myHeaders.append("Authorization", apiKey)
+  myHeaders.append("mode","no-cors")
 
   var requestOptions = {
     headers: myHeaders,
+    
   }
 
-  fetch(
-    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=84097&price=2",
-    requestOptions
-  )
+  fetch(yelpURL, requestOptions )
     .then((response) => response.json())
     .then(function (data) {
       console.log(data)
       let randomIndex = Math.floor(Math.random() * data.businesses.length)
 
-      restaurant.innerText = data.businesses[randomIndex].name
+      restaurant.innerText = data.businesses[randomIndex].name + ' ' + data.businesses[randomIndex].location.display_address
     })
 
     .catch((error) => console.log("error", error))
@@ -51,15 +75,21 @@ function fetchActivities() {
   //     console.log(error)
   // })
 }
-function doBoth (){
+function doAll (event){
+    event.preventDefault()
+    setZipcode()
     fetchActivities()
     fetchRestaurants()
 }
-document.querySelector(".date-ideas").addEventListener("click", fetchActivities)
 
-document
-  .querySelector(".restaurants")
-  .addEventListener("click", fetchRestaurants)
+function doBoth(){ 
+fetchActivities()
+fetchRestaurants()
+}
+
 
   document.querySelector('.do-both')
   .addEventListener('click', doBoth)
+
+  document.getElementById('zipcode-button')
+  .addEventListener('click', doAll)
